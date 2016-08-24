f319601e072b0e384a530f905c4b61384b8eb4a9
Ryan Fredlund <rfredlund13@gmail.com>
2016-08-24 12:15:22 -0500

    Correction: DMX out not working

--- midils/midils.py
+++ midils/midils.py
@@ -16,7 +16,6 @@ def main():
     # DMX is 1-indexed
     out_function = lambda channels: dmx.sendDMX([ch + 1 for ch in channels])
 #    out_function = vixenlog.send
-    dmx.sendDMX([255]*16)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     config_path = dir_path + '/mapping.json'
     mapper = Mapper(config_path)
